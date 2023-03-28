72ecac700431ee1d6de2f761db4acc5f62b06504
elyesjelaiel <122477529+elyesjelaiel@users.noreply.github.com>
2023-03-28 02:08:29 +0200

    Update solanaboard.py

--- solanaboard.py
+++ solanaboard.py
@@ -1,3 +1,4 @@
+
 import dash
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
@@ -23,6 +24,7 @@ date = pd.read_csv("date.txt",sep="\t",header=None, names=["date"])
 
 # Transformer les données en un DataFrame Pandas
 date["date"] = pd.to_datetime(date["date"])
+
 # Initialiser l'application Dash
 
 app = dash.Dash(__name__)
@@ -63,42 +65,30 @@ html.Div(id='daily-metrics')
 
 ])
 def calculate_daily_metrics():
-df['solana'] = df['solana'].str.replace('$','').astype(float)
-df['returns']=df["solana"].pct_change()
-daily_returns = round(df['returns'].iloc[-1]*100,2)
-daily_volatility = round(df['returns'].std()*100,2)     
+df["solana"]=df["solana"].apply(lambda x: x[1:])
+df["solana"]=df["solana"].astype(float)
+daily_volatility = round(df['solana'].std()*100,2)
 today = df.iloc[-1]["solana"]
 yesterday=df.iloc[-2]["solana"]
-return daily_returns, daily_volatility, today, yesterday
+return daily_volatility, today, yesterday
 
 
-@app.callback(Output('solana-graph','figure'),[Input('interval-component','n_intervals')])
+@app.callback(Output('solana-graph','figure'),[Input('interval-component','n_intervals')]) 
 def update_price_graph(n):
 fig = go.Figure(data=go.Scatter(x=date["date"], y=df["solana"]))
 return fig
- Tableau des prix de Solana (en USD) sur les 30 derniers jours :
 
-    '''),
-dcc.Interval(id='interval-component', interval=5*60*1000, n_intervals=0),
 
-html.Div(children=[
-html.H2('Daily Metrics'),
-html.Div(id='daily-metrics')
-
+@app.callback(Output('daily-metrics','children'),[Input('interval-component', 'n_intervals')]) 
+def update_metrics(n):
+daily_volatility, today, yesterday= calculate_daily_metrics()
+text = html.Div([
+html.P('Daily Volatility (annualized): {:.2%}'.format(daily_volatility)),
+html.P('Price Today:{:.2f}'.format(today)),
+html.P('Price Yesterday: {:.2f}'.format(yesterday))
 ])
-
-])
-def calculate_daily_metrics():
-df['solana'] = df['solana'].str.replace('$','').astype(float)
-df['returns']=df["solana"].pct_change()
-daily_returns = round(df['returns'].iloc[-1]*100,2)
-daily_volatility = round(df['returns'].std()*100,2)     
-today = df.iloc[-1]["solana"]
-yesterday=df.iloc[-2]["solana"]
-return daily_returns, daily_volatility, today, yesterday
+return text
 
 
-@app.callback(Output('solana-graph','figure'),[Input('interval-component','n_intervals')])
-def update_price_graph(n):
-fig = go.Figure(data=go.Scatter(x=date["date"], y=df["solana"]))
-return fig
+if __name__ == '__main__':
+app.run_server(debug=True,host="0.0.0.0",port=8050)
